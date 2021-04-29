97e92728a61b292c60a3af24c104f6ae9e1d74f0
Sandor Gyapjas <execska@192.168.0.103>
2021-04-29 10:42:53 +0200

    Implement methods in Swordsman class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Swordsman.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Swordsman.java
@@ -1,3 +1,19 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class Swordsman {}
+public class Swordsman extends MilitaryUnit {
+    private boolean first;
+
+    public Swordsman(boolean armour) {
+        super(10, 100, armour);
+        this.first = true;
+    }
+
+    @Override
+    public void sufferDamage(int damage) {
+        if (first) {
+            first = false;
+        } else {
+            super.sufferDamage(damage);
+        }
+    }
+}
