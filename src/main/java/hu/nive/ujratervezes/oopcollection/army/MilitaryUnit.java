0fea06cc0e0afc5d9bed3ff1f39af403b2c98a40
Sandor Gyapjas <execska@192.168.0.103>
2021-04-29 10:41:12 +0200

    Implement methods in MilitaryUnit class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/MilitaryUnit.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/MilitaryUnit.java
@@ -1,3 +1,30 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public abstract class MilitaryUnit {}
+public abstract class MilitaryUnit {
+    private int damage;
+    private int life;
+    private boolean armour;
+
+    public MilitaryUnit(int damage, int life, boolean armour) {
+        this.damage = damage;
+        this.life = life;
+        this.armour = armour;
+    }
+
+    public int doDamage() {
+        return this.damage;
+    };
+
+    public void sufferDamage(int damage) {
+        this.life -= this.armour ? damage / 2 : damage;
+    }
+
+    public boolean isUsable() {
+        return this.life > 24;
+    }
+
+    public int getHitPoints() {
+        return this.life;
+    }
+
+}
