ba2b690d784f1f1e7dcfdd4a037338c6e74c462a
Sandor Gyapjas <execska@192.168.0.103>
2021-04-29 10:42:12 +0200

    Implement methods in Archer class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Archer.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Archer.java
@@ -1,3 +1,9 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class Archer {}
+public class Archer extends MilitaryUnit {
+
+    public Archer() {
+        super(20, 50, false);
+    }
+
+}
