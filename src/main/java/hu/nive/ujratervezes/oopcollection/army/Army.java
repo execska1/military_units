3e3e7681d6c7cd2c97a24af82ebae6d439ce2594
Sandor Gyapjas <execska@192.168.0.103>
2021-04-29 10:44:19 +0200

    Implement methods in Army class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
@@ -1,3 +1,29 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class Army {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Army {
+    private List<MilitaryUnit> army;
+
+    public Army() {
+        this.army = new ArrayList<>();
+    }
+
+    public void addUnit(MilitaryUnit militaryUnit) {
+        army.add(militaryUnit);
+    }
+
+    public void damageAll(int damage) {
+        army.forEach(mU -> mU.sufferDamage(damage));
+        army.removeIf(mU -> !mU.isUsable());
+    }
+
+    public int getArmyDamage() {
+        return (int)army.stream().mapToInt(MilitaryUnit::doDamage).sum();
+    }
+
+    public int getArmySize() {
+        return army.size();
+    }
+}
