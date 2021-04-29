ccc3e432400b6da5cc081b7a70ef1ddb906f0a09
Sandor Gyapjas <execska@192.168.0.103>
2021-04-29 10:43:38 +0200

    Implement methods in HeavyCavalary class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/HeavyCavalry.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/HeavyCavalry.java
@@ -1,3 +1,20 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class HeavyCavalry {}
+public class HeavyCavalry extends MilitaryUnit {
+    private boolean first;
+
+    public HeavyCavalry() {
+        super(20, 150, true);
+        this.first = true;
+    }
+
+    @Override
+    public int doDamage() {
+        if (first) {
+            first = false;
+            return 3 * super.doDamage();
+        } else {
+            return super.doDamage();
+        }
+    }
+}
